c2f77bbe694f6106fa7722611f1a8566293a7b64
Clint Maruti <cmaruti93@gmail.com>
2021-07-03 21:36:26 +0300

    [Displaying a log file in real time]

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -3,10 +3,12 @@ const logWindow = document.querySelector("#log-window");
 const filePath = document.getElementById("logFilePath");
 
 connection.onopen = () => {
-    filePath ? true : connection.send(filePath) 
+  if (filePath) {
+    connection.send(filePath);
+  }
 };
 
 connection.onmessage = (event) => {
-    const logs = event.data.split("\n").join("<hr>")
-    logWindow.innerHTML = logs
+  const logs = event.data.split("\n").join("<hr>");
+  logWindow.innerHTML = logs;
 };
